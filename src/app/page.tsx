55dc3a81ae6a1a4b2e7fa78b4f01102d17418f78
xidik12 <xidik12@gmail.com>
2024-11-13 15:23:58 +0700

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -46,7 +46,7 @@ export default function Page() {
             text="Sign in to transact"
           />
         )}
-        <Checkout productId='my-product-id' > 
+        <Checkout productId='c1221f5f-0365-4a27-8859-c73fe621461a' > 
           <CheckoutButton coinbaseBranded/> // set coinbaseBranded for branding
           <CheckoutStatus />
         </Checkout>
